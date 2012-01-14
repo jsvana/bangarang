62d5cb3c70d5bb14b73d04031156a9c0e181da94
Jay Vana <jaysvana@gmail.com>
2012-01-14 15:35:31 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bangarang::Application.config.secret_token = 'a3d77bc4f87744a420d6317d0ccaf286c3f3bf05834c605b6118c244bc92c5126fda82d167484dd2d55563ce0a8a7a909981b9ea1e9027e1c9e54e302ed7aafd'

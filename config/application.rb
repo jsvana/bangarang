c80fdd39bfbf9297d6bbc84c71b8686ebec881fd
Jay Vana <jsvana@mtu.edu>
2012-02-04 20:27:25 -0500

    Updating...

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,10 @@ module Bangarang
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
+	config.generators do |g|
+      g.template_engine :haml
+    end
+
     # Enable the asset pipeline
     config.assets.enabled = true
 

0e223b84598028bbb40e1e12893bc96e82d3f3d6
Jay Vana <jsvana@mtu.edu>
2012-02-12 14:33:17 -0500

    Random commit.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,10 @@
 class Movie < ActiveRecord::Base
+
+	def self.update_movies
+		search = Imdb::Search.new('Avatar')
+
+		movie = Imdb::Movie.new(search.movies[0].id)
+
+		puts movie.title
+	end
 end

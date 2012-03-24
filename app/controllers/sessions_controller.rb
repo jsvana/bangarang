3bce563396d61567639c9ec28d7e44222f524333
Jay Vana <jaysvana@gmail.com>
2012-03-24 16:13:00 -0400

    Changed login page layout.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
 class SessionsController < ApplicationController
+
   def new
+    render 'new', layout: 'login'
   end
 
   def create
@@ -10,13 +12,13 @@ class SessionsController < ApplicationController
     user = User.find_by_username(params[:username])
     if user && !user.approved
       flash.now.alert = "You have not yet been approved"
-      render "new"
+      render "new", layout: 'login'
     elsif user && user.approved? && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_url, :notice => "Logged in!"
     else
       flash.now.alert = "Invalid username or password"
-      render "new"
+      render "new", layout: 'login'
     end
   end
 
